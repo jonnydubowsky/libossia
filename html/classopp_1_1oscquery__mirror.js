411dcee3fd857a15ac2726b1f55c422fc62d714c
Travis CI <travis@travis-ci.org>
2018-10-05 14:02:58 +0000

    Deploy code docs to GitHub Pages Travis build: 3050
    Commit: af7eebd093a0eeddee9da3a8c82ec278ee20df33

---
+++ html/classopp_1_1oscquery__mirror.js
@@ -0,0 +1,8 @@
+var classopp_1_1oscquery__mirror =
+[
+    [ "oscquery_mirror", "classopp_1_1oscquery__mirror.html#abc466dd1e77aa698e18b48d49272e161", null ],
+    [ "~oscquery_mirror", "classopp_1_1oscquery__mirror.html#a4ad453864c4828299d74ff5af4f2a58f", null ],
+    [ "get_root_node", "classopp_1_1oscquery__mirror.html#a3d988f2b0c4e855ac67e8f7ef10bd0d2", null ],
+    [ "refresh", "classopp_1_1oscquery__mirror.html#a580e75ad9e7c29b95aeb023519a41aed", null ],
+    [ "reconnect", "classopp_1_1oscquery__mirror.html#aee6acd9af30c6c6570f0f152af597ec7", null ]
+];
---
+++ html/classopp_1_1oscquery__server.js
@@ -0,0 +1,14 @@
+var classopp_1_1oscquery__server =
+[
+    [ "oscquery_server", "classopp_1_1oscquery__server.html#a7215addc739164ba0c07e18f785ad043", null ],
+    [ "oscquery_server", "classopp_1_1oscquery__server.html#affce415f4f18642715189f6b4a32a85c", null ],
+    [ "~oscquery_server", "classopp_1_1oscquery__server.html#a034e2d427c3dbc5cd858f9e02c713fbd", null ],
+    [ "setup", "classopp_1_1oscquery__server.html#ad6b302664298447f88ed0b48becdc97a", null ],
+    [ "get_root_node", "classopp_1_1oscquery__server.html#a2a31aabe97b3fc24303bf09f2dfac670", null ],
+    [ "set_echo", "classopp_1_1oscquery__server.html#a46468df7b1ac7dfb08e37e5804decff8", null ],
+    [ "get_echo", "classopp_1_1oscquery__server.html#a2df56cba1315479d5d68e0faff869218", null ],
+    [ "set_connection_callback", "classopp_1_1oscquery__server.html#a7c2edc56020e5fc74fc6209170559007", null ],
+    [ "remove_connection_callback", "classopp_1_1oscquery__server.html#acaa1f3fe5ac38ec7d8002db0fd115f97", null ],
+    [ "set_disconnection_callback", "classopp_1_1oscquery__server.html#a322f5b747d25d805ee5088de9df3ae6d", null ],
+    [ "remove_disconnection_callback", "classopp_1_1oscquery__server.html#a1a9ee42f3a51e7b3f79fbc91d736a2ed", null ]
+];
